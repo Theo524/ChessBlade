7f2647d9142719cd6f88218585d65108ce624b1e
Theo524 <theophilusbrownjacob524@gmail.com>
2021-06-10 16:24:10 +0100

    Changes to settings.py: - Made all fields empty by default - Added multiple informative feedback - The user can no longer submit empty fields - Removed reset() method as it is not needed

--- apps/chess_app/objects/settings.py
+++ apps/chess_app/objects/settings.py
@@ -11,6 +11,7 @@ class Settings(Tk):
         # attributes
         self.title('Settings')
         self.resizable(0, 0)
+        self.protocol('WM_DELETE_WINDOW', self.confirm_exit)
 
         # All the game settings will be contained within this notebook
         notebook = ttk.Notebook(self)
@@ -35,20 +36,40 @@ class Settings(Tk):
 
     def save_settings(self):
         """Saves settings too file"""
+        requirements_not_met = 0
 
-        # Retrieving settings from the classes
+        # Retrieving settings data from the classes
         self.difficulty = self.general_settings.get_difficulty()
+        if type(self.difficulty) != str or len(self.difficulty)<1:
+            requirements_not_met += 1
+
         self.time = self.general_settings.get_time()
+        if type(self.time) != str or len(self.time) != 8:
+            requirements_not_met += 1
+
         self.game_type = self.general_settings.get_gamemode()
+        if type(self.game_type) != str or len(self.game_type) <1:
+            requirements_not_met += 1
+
         self.player_color = self.customization_settings.get_piece_color()
+        if self.player_color not in ['black', 'white']:
+            requirements_not_met += 1
 
         if self.player_color == 'black':
             self.opponent_color = 'white'
         else:
             self.opponent_color = 'black'
 
+        if self.opponent_color not in ['black', 'white']:
+            requirements_not_met += 1
+
         self.border_color = self.customization_settings.get_border_color()
+        if type(self.border_color) != str or len(self.border_color) < 1:
+            requirements_not_met += 1
+
         self.board_color = self.customization_settings.get_board_color()
+        if type(self.board_color) != str or len(self.board_color) < 1:
+            requirements_not_met += 1
 
         # get game mode
         with open(os.getcwd() + '\\apps\\login_system_app\\temp\\mode.txt', 'r') as f:
@@ -58,31 +79,34 @@ class Settings(Tk):
         confirm = messagebox.askyesno('Confirmation', 'Are you sure you want to apply these settings?')
         # if the user accepts, store settings, show informative feedback and destroy settings window
         if confirm:
-            print(f'{self.difficulty}-{self.time}-{self.game_type}-{self.player_color}-{self.opponent_color}'
-                            f'-{self.border_color}-{self.board_color}')
-            # if user is in guest mode, apply new settings to system
-            if game_mode == 'guest':
-                with open(os.getcwd() + '\\apps\\chess_app\\all_settings\\guest\\default_game_settings.csv', 'w') as f:
-                    f.write('Game_difficulty, time, game_mode, player_piece_color, opponent_piece_color, border_color,'
-                            'board_color\n')
-                    f.write(f'{self.difficulty}-{self.time}-{self.game_type}-{self.player_color}-{self.opponent_color}'
-                            f'-{self.border_color}-{self.board_color}')
+            # first check there are no empty fields
+            if requirements_not_met == 0:
+                # if user is in guest mode, apply new settings to system
+                if game_mode == 'guest':
+                    with open(os.getcwd() + '\\apps\\chess_app\\all_settings\\guest\\default_game_settings.csv', 'w') as f:
+                        f.write('Game_difficulty, time, game_mode, player_piece_color, opponent_piece_color, border_color,'
+                                'board_color\n')
+                        f.write(f'{self.difficulty}-{self.time}-{self.game_type}-{self.player_color}-{self.opponent_color}'
+                                f'-{self.border_color}-{self.board_color}')
 
-            # if user is in user mode, apply new settings to user account
-            if game_mode == 'user':
-                # apply changes to db
-                self.apply_settings_user_db()
-                # save the new settings in a file
-                with open(os.getcwd() + '\\apps\\chess_app\\all_settings\\user\\user_game_settings.csv', 'w') as f:
-                    f.write('Game_difficulty-time-game_mode-player_piece_color-opponent_piece_color-border_color-'
-                            'board_color\n')
-                    f.write(f'{self.difficulty}-{self.time}-{self.game_type}-{self.player_color}-{self.opponent_color}'
-                            f'-{self.border_color}-{self.board_color}')
+                # if user is in user mode, apply new settings to user account
+                if game_mode == 'user':
+                    # apply changes to db
+                    self.apply_settings_user_db()
+                    # save the new settings in a file
+                    with open(os.getcwd() + '\\apps\\chess_app\\all_settings\\user\\user_game_settings.csv', 'w') as f:
+                        f.write('Game_difficulty-time-game_mode-player_piece_color-opponent_piece_color-border_color-'
+                                'board_color\n')
+                        f.write(f'{self.difficulty}-{self.time}-{self.game_type}-{self.player_color}-{self.opponent_color}'
+                                f'-{self.border_color}-{self.board_color}')
 
-            # Confirmation and informative feedback
-            messagebox.showinfo('Success', 'Settings successfully saved. Start a new game to play with new settings.')
-            # destroy settings window
-            self.destroy()
+                # Confirmation and informative feedback
+                messagebox.showinfo('Success', 'Settings successfully saved.'
+                                               ' Start a new game to play with the new settings.')
+                # destroy settings window
+                self.destroy()
+            else:
+                messagebox.showerror('Error', f'You left {requirements_not_met} empty field(s)')
         else:
             pass
 
@@ -118,14 +142,12 @@ class Settings(Tk):
                        'board_color': self.board_color,
                        'user': username})
 
-    def reset(self):
-        """Default settings values"""
+    def confirm_exit(self):
+        """Confirms whether user wants to exit window"""
 
-        self.general_settings.difficulty_var.set(1)
-        self.general_settings.gamemode_var.set(1)
-        self.customization_settings.border_color_var.set('black')
-        self.customization_settings.player_color_var.set('black')
-        self.customization_settings.board_color_var.set('brown')
+        confirm = messagebox.askyesno('Error', 'Are you sure you want to exit?\nAll settings will be lost!')
+        if confirm:
+            self.destroy()
 
 
 class GeneralSettings(ttk.Frame):
@@ -163,17 +185,14 @@ class GeneralSettings(ttk.Frame):
                                  command=self.get_difficulty)
         expert.pack(side=LEFT)
 
-        # Set default to 1 (novice)
-        self.difficulty_var.set(1)
-
         # --------------TIME--------------
         time_frame = ttk.LabelFrame(self, text="Time")
         time_frame.pack(fill="both", expand="yes", pady=5, padx=5)
 
         # variables
-        self.time_in_hours = StringVar(value=2)
-        self.time_in_minutes = StringVar(value=30)
-        self.time_in_seconds = StringVar(value=0)
+        self.time_in_hours = StringVar()
+        self.time_in_minutes = StringVar()
+        self.time_in_seconds = StringVar()
 
         # hours entry
         Label(time_frame, text='Hours').pack(side=LEFT)
@@ -182,13 +201,13 @@ class GeneralSettings(ttk.Frame):
 
         # minutes entry
         Label(time_frame, text='Minutes').pack(side=LEFT)
-        time_limit_minutes = ttk.Spinbox(time_frame, width=3, from_=0, to=60, textvariable=self.time_in_minutes,
+        time_limit_minutes = ttk.Spinbox(time_frame, width=3, from_=1, to=60, textvariable=self.time_in_minutes,
                                          wrap=True)
         time_limit_minutes.pack(side=LEFT, padx=3)
 
         # seconds entry
         Label(time_frame, text='Seconds').pack(side=LEFT)
-        time_limit_seconds = ttk.Spinbox(time_frame, width=3, from_=0, to=60, textvariable=self.time_in_seconds,
+        time_limit_seconds = ttk.Spinbox(time_frame, width=3, from_=30, to=60, textvariable=self.time_in_seconds,
                                          wrap=True)
         time_limit_seconds.pack(side=LEFT, padx=3)
 
@@ -212,8 +231,6 @@ class GeneralSettings(ttk.Frame):
                                    value=2,
                                    command=self.get_gamemode)
         computer.pack(side=LEFT)
-        # set default game mode to 'two player'
-        self.gamemode_var.set(1)
 
     def get_difficulty(self):
         """Get difficulty"""
@@ -278,9 +295,12 @@ class CustomizationSettings(ttk.Frame):
         white = ttk.Radiobutton(player_color_frame,
                                 text='white',
                                 variable=self.player_color_var,
-                                value=0,
+                                value=2,
                                 command=self.get_piece_color)
         white.pack(side=LEFT)
+
+        # this way it is set to none
+        self.player_color_var.set(0)
 
         # ----------------BORDER_COLORS-----------------
         border_color_frame = ttk.LabelFrame(self, text='Border colors')
@@ -314,7 +334,7 @@ class CustomizationSettings(ttk.Frame):
 
     def get_piece_color(self):
         """Get the piece color"""
-        if self.player_color_var.get() == 0:
+        if self.player_color_var.get() == 2:
             return 'white'
 
         elif self.player_color_var.get() == 1:
@@ -327,4 +347,3 @@ class CustomizationSettings(ttk.Frame):
     def get_border_color(self):
         """Get the border color"""
         return self.border_colors.get()
-
