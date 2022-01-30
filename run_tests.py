3b9e9b0f2c1f1edeb6e5deee22051759a99ab449
Theo524 <theophilusbrownjacob524@gmail.com>
2022-01-30 15:03:52 +0000

    Add tests

---
+++ run_tests.py
@@ -0,0 +1,27 @@
+import tkinter
+
+import tests.loginTest as Login
+import tests.mainTest as Files
+import tests.databaseTest as Database
+import tests.boardTest as Chess
+
+import unittest
+print
+# test some login system functions
+suite = unittest.TestLoader().loadTestsFromModule(Login)
+unittest.TextTestRunner(verbosity=2).run(suite)
+
+# test all files needed are in the program
+suite = unittest.TestLoader().loadTestsFromModule(Files)
+unittest.TextTestRunner(verbosity=2).run(suite)
+
+# test database browsing commands
+suite = unittest.TestLoader().loadTestsFromModule(Database)
+unittest.TextTestRunner(verbosity=2).run(suite)
+
+# test board obj
+suite = unittest.TestLoader().loadTestsFromModule(Chess)
+unittest.TextTestRunner(verbosity=2).run(suite)
+
+if __name__ == '__main__':
+    unittest.main()
